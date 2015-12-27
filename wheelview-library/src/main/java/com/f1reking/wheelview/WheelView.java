4b94e4edaaa49adb78188213181f15e88a7eb98c
F1ReKing <284061805@qq.com>
2015-12-27 11:19:11 +0800

    first commit

---
+++ wheelview-library/src/main/java/com/f1reking/wheelview/WheelView.java
@@ -0,0 +1,23 @@
+package com.f1reking.wheelview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by HuangYH on 2015/12/27.
+ */
+public class WheelView extends ScrollView {
+
+    public WheelView(Context context) {
+        super(context);
+    }
+
+    public WheelView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public WheelView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
